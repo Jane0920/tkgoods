d879bf504d8678a820656c9fcef386b67b47af17
Jane0920 <736845367@qq.com>
2017-10-14 19:56:03 +0800

    Initial commit

---
+++ src/main/java/com/yys/TkgoodsApplication.java
@@ -0,0 +1,12 @@
+package com.yys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkgoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkgoodsApplication.class, args);
+	}
+}
